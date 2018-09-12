e7e9311f745870fb30b648be2c66d4c2788f6f33
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-12 12:41:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 function showRepositories() {
-  console.log(this.responseText);
-  let repoList = '<ul>';
-  for (var i = 0; i < this.responseText.length; i++) {
-    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
-  }
-  repoList += '</ul>';
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(
+      r =>
+        '<li>' +
+        r.name +
+        ' - <a href="#" data-repo="' +
+        r.name +
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
+    .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
- 
+
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
